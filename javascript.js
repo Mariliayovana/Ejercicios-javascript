b477679481fd56a386858219faa354b3b5994bc4
Marilia Yovana <mariliayovi@gmail.com>
2022-05-10 21:04:17 -0500

    mostrando con el DOM para la primera pantal del ingreso del cliente, y obteniendo el valor del producto del JSON para el carrito.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,26 @@
+//función para la bienvenida
 function obtenerNumeroAleatorio(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
-
 let saludos =[ "Hola", "Cómo estás", "Bienvenido", "Que gusto de verte"]
 
-function solicitarNombre() {
+let botonUno = document.getElementById("botoncito");
+botonUno.addEventListener("click", function solicitarNombre(e) {
+  e.preventDefault()
+  let nombreIngresado = document.getElementById("nombreCliente").value;
+  let apellidoIngresa = document.getElementById("apellido").value;
+  
   let posSaludo = obtenerNumeroAleatorio(0,saludos.length);
   const saludoAleatorio = saludos[posSaludo]
-  let nombreIngresado = prompt("Ingresar su nombre");
-  let apellidoIngresa = prompt("Ingresa su apellido");
-  alert(saludoAleatorio + " " + nombreIngresado + " " + apellidoIngresa);
-}
-solicitarNombre();
+  if ((nombreIngresado == "") || (apellidoIngresa == "")) {
+    alert("Ingrese su Nombre y Apellido");
+  } else{
+    let nombreCompleto = document.getElementById("nombreTitulo");
+    nombreCompleto.innerHTML = saludoAleatorio + " " + nombreIngresado + " " + apellidoIngresa;
+  }
+  inicio.reset();
+})
+//función para ingresar a los productos
 
 let container = document.getElementById("contenedor");
 
@@ -23,7 +32,7 @@ container.innerHTML = '';
 for (const almacen  of producto){
   container.innerHTML += `<h3> ID: ${almacen.id} </h3>
                           <p> PRODUCTO: ${almacen.nombre} </p>
-                          <p> PRECIO: ${almacen.precio} </p>`;
+                          <p> PRECIO: $${almacen.precio} </p>`;
 }
 
 
@@ -49,66 +58,101 @@ boton.addEventListener("click",function (evento) {
   }
 });
 
-// let miFormulario = document.getElementById("formulario");
-// miFormulario.addEventListener("submit", validarFormulario);
+let miFormulario = document.getElementById("formulario");
+miFormulario.addEventListener("submit", validarFormulario);
 
-// function validarFormulario (e) {
-//   e.preventDefault();
-//   let formulario = e.target
-//   formulario.innerHTML = `<p> Producto: ${formulario.children.length + 1} </p>`
-// }
+function validarFormulario (e) {
+  e.preventDefault();
+  let formulario = e.target
+  formulario.innerHTML = `<p> Producto: ${formulario.children.length + 1} </p>`
+}
+// almacenado en le localStorage para mi carrito
+
+const productoAlmacenado = [
+  { id: 1, producto: "fideo", precio: 30 },
+  { id: 2, producto: "té", precio: 20 },
+  { id: 3, producto: "pan", precio: 60 },
+  { id: 4, producto: "atún", precio: 80 },
+  { id: 5, producto: "gelatina", precio: 10 },
+]
+
+const guardarLocal = (clave, valor) =>{
+  localStorage.setItem(clave, valor)
+};
+for (const product of productoAlmacenado){
+  guardarLocal(product.id, JSON.stringify(product));
+}
+guardarLocal("listaProductos", JSON.stringify(productoAlmacenado));
+
+class Producto {
+  constructor(obj) {
+    this.nombre = obj.producto.toUpperCase();
+    this.precio = parseFloat(obj.precio);
+  }
+  sumaIva(){
+    this.precio = this.precio * 1.20;
+  }
+}
+const almacenando = JSON.parse(localStorage.getItem("listaProductos"));
+const productos = [];
+for (const objeto of almacenando)
+      productos.push(new Producto(objeto));
+      console.log(productos)
+for (const product of productos)
+      product.sumaIva();
+      
+      console.log(productos)
 
 
 
-/*
 // INGRESANDO EL PRECIO DEL PRODUCTO Y SUS DESCUENTOS
 
-const suma = (a,b) => a + b;
-const resta = (a,b) => a - b;
-const iva = x  => x * 0.21;
-let precioProducto = Number(prompt("Ingrese el precio del producto"));
-let descuento = x => x * 0.3;
-let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), descuento(precioProducto));
-console.log(nuevoPrecio)
+// const suma = (a,b) => a + b;
+// const resta = (a,b) => a - b;
+// const iva = x  => x * 0.21;
+// let precioProducto = Number(prompt("Ingrese el precio del producto"));
+// let descuento = x => x * 0.3;
+// let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), descuento(precioProducto));
+// console.log(nuevoPrecio)
 
-// LOS PRODUCTOS NUEVOS
+// // LOS PRODUCTOS NUEVOS
 
-class producto {
-  constructor(nombre, precio) {
-    this.nombre = nombre.toUpperCase();
-    this.precio = parseFloat(precio);
-    this.vendido = false;
-  }
-  sumarIva(){
-    this.precio = this.precio * 0.18
-  }
-}
+// class producto {
+//   constructor(nombre, precio) {
+//     this.nombre = nombre.toUpperCase();
+//     this.precio = parseFloat(precio);
+//     this.vendido = false;
+//   }
+//   sumarIva(){
+//     this.precio = this.precio * 0.18
+//   }
+// }
 
-const ingresa = [];
-ingresa.push(new producto ("Zanahoria", "9.5"));
-ingresa.push(new producto ("Tomate", "8.5"));
-ingresa.push(new producto ("zapallo", "10"));
-ingresa.push(new producto ("Lechuga", "10"));
+// const ingresa = [];
+// ingresa.push(new producto ("Zanahoria", "9.5"));
+// ingresa.push(new producto ("Tomate", "8.5"));
+// ingresa.push(new producto ("zapallo", "10"));
+// ingresa.push(new producto ("Lechuga", "10"));
 
-for (const guardar of ingresa)
-    guardar.sumarIva();
+// for (const guardar of ingresa)
+//     guardar.sumarIva();
 
-    console.log(ingresa)
+//     console.log(ingresa)
 
-const abarrote = ["arroz", "azucar", "fideo", "té", "maiz"];
-// console.log(abarrote.indexOf("azucar"));
-const eliminar = (nom) => {
-  let proEncontrado = abarrote.indexOf(nom);
+// const abarrote = ["arroz", "azucar", "fideo", "té", "maiz"];
+// // console.log(abarrote.indexOf("azucar"));
+// const eliminar = (nom) => {
+//   let proEncontrado = abarrote.indexOf(nom);
 
-  if (proEncontrado != -1) {
-    abarrote.splice(proEncontrado, 1)
-  }
-  return abarrote
-}
-let IngresoProducto = prompt("Ingrese el producto que desea eliminar: " + abarrote);
-let elProducto = eliminar(IngresoProducto);
-console.log(elProducto);
-*/
+//   if (proEncontrado != -1) {
+//     abarrote.splice(proEncontrado, 1)
+//   }
+//   return abarrote
+// }
+// let IngresoProducto = prompt("Ingrese el producto que desea eliminar: " + abarrote);
+// let elProducto = eliminar(IngresoProducto);
+// console.log(elProducto);
+
 
 
 
