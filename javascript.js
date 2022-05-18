f56f0ed1ab9f3b7c79eea32a05441019ac143f17
Marilia Yovana <mariliayovi@gmail.com>
2022-05-17 21:07:43 -0500

    mostrando con el DOM para la primera pantal del ingreso del cliente, y obteniendo el valor del producto del JSON para el carrito.

--- javascript.js
+++ javascript.js
@@ -1,74 +1,7 @@
-//función para la bienvenida
-function obtenerNumeroAleatorio(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-let saludos =[ "Hola", "Cómo estás", "Bienvenido", "Que gusto de verte"]
 
-let botonUno = document.getElementById("botoncito");
-botonUno.addEventListener("click", function solicitarNombre(e) {
-  e.preventDefault()
-  let nombreIngresado = document.getElementById("nombreCliente").value;
-  let apellidoIngresa = document.getElementById("apellido").value;
-  
-  let posSaludo = obtenerNumeroAleatorio(0,saludos.length);
-  const saludoAleatorio = saludos[posSaludo]
-  if ((nombreIngresado == "") || (apellidoIngresa == "")) {
-    alert("Ingrese su Nombre y Apellido");
-  } else{
-    let nombreCompleto = document.getElementById("nombreTitulo");
-    nombreCompleto.innerHTML = saludoAleatorio + " " + nombreIngresado + " " + apellidoIngresa;
-  }
-  inicio.reset();
-})
-//función para ingresar a los productos
-
-let container = document.getElementById("contenedor");
-
-const producto = [
-  {id: 1, nombre: "Azucar", precio: 20},
-  {id: 2, nombre: "Arroz", precio: 15},
-];       
-container.innerHTML = '';
-for (const almacen  of producto){
-  container.innerHTML += `<h3> ID: ${almacen.id} </h3>
-                          <p> PRODUCTO: ${almacen.nombre} </p>
-                          <p> PRECIO: $${almacen.precio} </p>`;
-}
-
-
-let boton = document.getElementById("boton");
-
-const todosLosProductos = [];
-
-boton.addEventListener("click",function (evento) {
-  evento.preventDefault()
-  let nombre = document.getElementById("nombre").value;
-  let precio = document.getElementById("precios").value;
-  if ((nombre == "" ) || (precio == "")) {
-    alert("llene todos los campos del producto");
-  }else {
-  todosLosProductos.push({ nombre, precio})
-  formulario.reset();
-  for (const suma  of todosLosProductos) {
-
-    container.innerHTML += `<h3> producto ingresado</h3>
-                            <p> PRODUCTO: ${suma.nombre} </p>
-                            <p> CANTIDAD: ${suma.precio} </p>`;
-    }
-  }
-});
-
-let miFormulario = document.getElementById("formulario");
-miFormulario.addEventListener("submit", validarFormulario);
-
-function validarFormulario (e) {
-  e.preventDefault();
-  let formulario = e.target
-  formulario.innerHTML = `<p> Producto: ${formulario.children.length + 1} </p>`
-}
 // almacenado en le localStorage para mi carrito
 
-const productoAlmacenado = [
+const productoAlmace = [
   { id: 1, producto: "fideo", precio: 30 },
   { id: 2, producto: "té", precio: 20 },
   { id: 3, producto: "pan", precio: 60 },
@@ -79,10 +12,10 @@ const productoAlmacenado = [
 const guardarLocal = (clave, valor) =>{
   localStorage.setItem(clave, valor)
 };
-for (const product of productoAlmacenado){
+for (const product of productoAlmace){
   guardarLocal(product.id, JSON.stringify(product));
 }
-guardarLocal("listaProductos", JSON.stringify(productoAlmacenado));
+guardarLocal("listaProductos", JSON.stringify(productoAlmace));
 
 class Producto {
   constructor(obj) {
@@ -107,53 +40,91 @@ for (const product of productos)
 
 // INGRESANDO EL PRECIO DEL PRODUCTO Y SUS DESCUENTOS
 
-// const suma = (a,b) => a + b;
-// const resta = (a,b) => a - b;
-// const iva = x  => x * 0.21;
-// let precioProducto = Number(prompt("Ingrese el precio del producto"));
-// let descuento = x => x * 0.3;
-// let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), descuento(precioProducto));
-// console.log(nuevoPrecio)
+/*const suma = (a,b) => a + b;
+const resta = (a,b) => a - b;
+const iva = x  => x * 0.21;
+let precioProducto = Number(prompt("Ingrese el precio del producto"));
+let descuento = x => x * 0.3;
+let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), descuento(precioProducto));
+console.log(nuevoPrecio)
+*/
+// LOS PRODUCTOS NUEVOS
+/*
+class producto {
+  constructor(nombre, precio) {
+    this.nombre = nombre.toUpperCase();
+    this.precio = parseFloat(precio);
+    this.vendido = false;
+  }
+  sumarIva(){
+    this.precio = this.precio * 0.18
+  }
+}
 
-// // LOS PRODUCTOS NUEVOS
+const ingresa = [];
+ingresa.push(new producto ("Zanahoria", "5.5"));
+ingresa.push(new producto ("Tomate", "4.5"));
+ingresa.push(new producto ("zapallo", "3.5"));
+ingresa.push(new producto ("cebolla", "2.5"));
 
-// class producto {
-//   constructor(nombre, precio) {
-//     this.nombre = nombre.toUpperCase();
-//     this.precio = parseFloat(precio);
-//     this.vendido = false;
-//   }
-//   sumarIva(){
-//     this.precio = this.precio * 0.18
-//   }
+for (const guardar of ingresa)
+    guardar.sumarIva();
+
+    console.log(ingresa)
+
+const abarrote = ["arroz", "azucar", "fideo", "té", "maiz"];
+// console.log(abarrote.indexOf("azucar"));
+const eliminar = (nom) => {
+  let proEncontrado = abarrote.indexOf(nom);
+
+  if (proEncontrado != -1) {
+    abarrote.splice(proEncontrado, 1)
+  }
+  return abarrote
+}
+let IngresoProducto = prompt("Ingrese el producto que desea eliminar: " + abarrote);
+let elProducto = eliminar(IngresoProducto);
+console.log(elProducto);
+*/
+
+// const productos = [
+//   {id: 1, nombre: "Azucar", precio: 20},
+//   {id: 2, nombre: "Arroz", precio: 15},
+// ];       
+// container.innerHTML = '';
+// for (const almacen  of productos){
+//   container.innerHTML += `<h3> ID: ${almacen.id} </h3>
+//                           <p> PRODUCTO: ${almacen.nombre} </p>
+//                           <p> PRECIO: $${almacen.precio} </p>`;
 // }
 
-// const ingresa = [];
-// ingresa.push(new producto ("Zanahoria", "9.5"));
-// ingresa.push(new producto ("Tomate", "8.5"));
-// ingresa.push(new producto ("zapallo", "10"));
-// ingresa.push(new producto ("Lechuga", "10"));
 
-// for (const guardar of ingresa)
-//     guardar.sumarIva();
 
-//     console.log(ingresa)
+// const todosLosProductos = [];
 
-// const abarrote = ["arroz", "azucar", "fideo", "té", "maiz"];
-// // console.log(abarrote.indexOf("azucar"));
-// const eliminar = (nom) => {
-//   let proEncontrado = abarrote.indexOf(nom);
+// boton.addEventListener("click",function (evento) {
+//   evento.preventDefault()
+//   let nombre = document.getElementById("nombre").value;
+//   let precio = document.getElementById("precios").value;
+//   if ((nombre == "" ) || (precio == "")) {
+//     alert("llene todos los campos del producto");
+//   }else {
+//   todosLosProductos.push({ nombre, precio})
+//   formulario.reset();
+//   for (const suma  of todosLosProductos) {
 
-//   if (proEncontrado != -1) {
-//     abarrote.splice(proEncontrado, 1)
+//     container.innerHTML += `<h3> producto ingresado</h3>
+//                             <p> PRODUCTO: ${suma.nombre} </p>
+//                             <p> CANTIDAD: ${suma.precio} </p>`;
+//     }
 //   }
-//   return abarrote
+// });
+
+// miFormulario.addEventListener("submit", validarFormulario);
+// function validarFormulario (e) {
+//   e.preventDefault();
+//   let formulario = e.target
+//   formulario.innerHTML = `<p> Producto: ${formulario.children.length + 1} </p>`
 // }
-// let IngresoProducto = prompt("Ingrese el producto que desea eliminar: " + abarrote);
-// let elProducto = eliminar(IngresoProducto);
-// console.log(elProducto);
-
-
-
 
 
---
+++ main.js
@@ -0,0 +1,89 @@
+// manejo del DOM
+let botonUno = document.getElementById("botoncito");
+let nombreIngresado = document.getElementById("nombreCliente");
+let contrasena = document.getElementById("password");
+let pantallaLogin = document.getElementById("login");
+let error = document.getElementById("error");
+let contenedor = document.getElementById("contenedor");
+let boton = document.getElementById("boton");
+let miFormulario = document.getElementById("formulario");
+let carro = document.getElementById("carro");
+
+
+//función de Login
+function obtenerNumeroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+let saludos =[ "Hola", "Cómo estás", "Bienvenido", "Que gusto de verte"]
+
+botonUno.addEventListener("click", function solicitarNombre(e) {
+  e.preventDefault()
+  let posSaludo = obtenerNumeroAleatorio(0,saludos.length);
+  const saludoAleatorio = saludos[posSaludo]
+  if (nombreIngresado.value == "MARIA" && contrasena.value == "12345") {
+    let nombreCompleto = document.getElementById("nombreTitulo");
+    nombreCompleto.innerHTML = saludoAleatorio + " " + nombreIngresado.value;
+    pantallaLogin.classList.toggle('ocultar');
+  } else{
+    error.innerHTML = "Nombre o Contraseña incorrecta";
+  
+  }
+  inicio.reset();
+})
+const productoAlmacenado = [
+    { id: 1, producto: "Tomate", precio : 4.5 , imagen: "/imagen/tomates.jpeg", tipo: "online", valor: 0, resta: "-", suma: "+" , agregar: "Agregar"},
+    { id: 2, producto: "Zanahoria", precio: 3.5, imagen:"/imagen/zanahorias.jpeg", tipo: "online", valor: 0, resta: "-", suma: "+" , agregar: "Agregar" },
+    { id: 3, producto: "Zapallo", precio: 5.5, imagen: "/imagen/zapallo.jpeg", tipo: "online", valor: 0, resta: "-", suma: "+" , agregar: "Agregar"},
+    { id: 4, producto: "Cebolla", precio: 2.5, imagen:"/imagen/cebolla.jpeg", tipo: "online", valor: 0, resta: "-", suma: "+" , agregar: "Agregar" },
+    { id: 5, producto: "Col", precio: 6.7, imagen: "/imagen/col.jpeg", tipo: "online", valor: 0, resta: "-", suma: "+" , agregar: "Agregar" },
+  ];
+
+  const mostrarData = () => {
+    let mostrar = '';
+    for (let i = 0; i < productoAlmacenado.length; i++){
+      let llamando = `<div class="col">
+                        <div class="card h-100">
+                          <img src="${productoAlmacenado[i].imagen}" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">${productoAlmacenado[i].producto}</h5>
+                            <span>${productoAlmacenado[i].tipo}</span>
+                            <span> s/${productoAlmacenado[i].precio }</span>
+                          </div>
+                          <div class="text-center">
+                            <span id="valor${i}">${productoAlmacenado[i].valor}</span>
+                            <button class="boton resta btn btn-light">${productoAlmacenado[i].resta}</button>
+                            <button class="boton sumar btn btn-light">${productoAlmacenado[i].suma}</button>
+                            <button class="btn btn-light">${productoAlmacenado[i].agregar}</button>
+                          </div>
+                        </div>
+                      </div>`;
+                mostrar += llamando      
+    }
+    return mostrar
+
+  }
+  const llenarContenido = () => {
+    // console.log(contenedor)
+    contenedor.innerHTML = mostrarData();
+  };
+  llenarContenido()
+
+  // contador 
+const botones = document.querySelectorAll(".boton");
+
+let contador = Array(productoAlmacenado.length).fill(0);
+botones.forEach((boton, index) => {
+  boton.addEventListener("click", function(e){
+    e.preventDefault()
+    const estilos = e.currentTarget.classList;
+    const butonIndex = Math.trunc(index /2  )
+    const valor = document.getElementById("valor" + butonIndex);
+    if(estilos.contains("resta")&& (contador[butonIndex] > 0)) {
+      contador[butonIndex]--;
+    } else if (estilos.contains("sumar")){
+      contador[butonIndex]++;
+    }
+    valor.textContent = contador[butonIndex];
+  })
+})
+
